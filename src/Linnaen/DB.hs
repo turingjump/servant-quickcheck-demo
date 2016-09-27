9e287961fc74f1ab221706ea3fe1458e64327740
Julian K. Arni <jkarni@gmail.com>
2016-09-27 12:41:53 +0200

    more

--- src/Linnaen/DB.hs
+++ src/Linnaen/DB.hs
@@ -1,3 +1,8 @@
 module Linnaen.DB where
 
+import qualified Hasql.Decoder as Dec
 
+listFamily :: Query () Family
+listFamily = _
+  where
+    sql = "SELECT (family_name) FROM family;"
--- src/Linnaen/Types.hs
+++ src/Linnaen/Types.hs
@@ -4,28 +4,30 @@ import Data.Aeson (FromJSON, ToJSON)
 import qualified Data.Text as T
 import GHC.Generics (Generic)
 import Data.Int (Int32)
+import Hasql.Class (Encodable, Decodable)
+
 
 newtype Id = Id { getId :: Int32 }
-  deriving (Eq, Ord, Show, Read, Generic, FromJSON, ToJSON)
+  deriving (Eq, Ord, Show, Read, Generic, FromJSON, ToJSON, Encodable, Decodable)
 
 data Discoverer = Discoverer
   { _discovererFirstName :: T.Text
   , _discovererLastName :: T.Text
-  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON)
+  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON, Encodable, Decodable)
 
 data Family = Family
   { _familyName :: T.Text
   , _familyDiscoverer :: Maybe Id
-  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON)
+  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON, Encodable, Decodable)
 
 data Genus = Genus
   { _genusName :: T.Text
   , _genusDiscoverer :: Maybe T.Text
   , _genusFamily :: Id
-  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON)
+  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON, Encodable, Decodable)
 
 data Species = Species
   { _speciesName :: T.Text
   , _speciesDiscoverer :: Maybe T.Text
   , _speciesGenus :: Id
-  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON)
+  } deriving (Eq, Show, Read, Generic, FromJSON, ToJSON, Encodable, Decodable)
